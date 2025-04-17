b30a765920d8e1aa711f67a68c0b6350c895d67f
ThanaphomH <thanaphom.h@gmail.com>
2025-04-18 01:08:43 +0700

    feat: sanitize html input

--- src/common/pipe/sanitize.pipe.ts
+++ src/common/pipe/sanitize.pipe.ts
@@ -1,9 +1,21 @@
 import { PipeTransform, Injectable, ArgumentMetadata } from '@nestjs/common';
 import sanitize from 'mongo-sanitize';
+import sanitizeHtml from 'sanitize-html';
 
 @Injectable()
 export class SanitizePipe implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata) {
-    return sanitize(value);
+    // MongoDB sanitization (clean NoSQL queries)
+    const sanitizedMongoValue = sanitize(value);
+
+    // XSS sanitization (clean HTML/JS injection)
+    if (typeof sanitizedMongoValue === 'string') {
+      return sanitizeHtml(sanitizedMongoValue, {
+        allowedTags: [],
+        allowedAttributes: {},
+      });
+    }
+
+    return sanitizedMongoValue;
   }
 }
