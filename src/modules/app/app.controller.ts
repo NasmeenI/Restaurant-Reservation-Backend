2e4d20a2f77e184ca6bb00d37dff1add12a8f577
ThanaphomH <thanaphom.h@gmail.com>
2025-04-19 00:26:49 +0700

    feat: health check swagger description

--- src/modules/app/app.controller.ts
+++ src/modules/app/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from 'src/modules/app/app.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('app')
 @Controller()
@@ -8,6 +8,13 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Hello world for health check' })
+  @ApiResponse({
+    status: 200,
+    description: 'Hello world',
+    type: String,
+    example: 'Hello World!',
+  })
   getHello(): string {
     return this.appService.getHello();
   }
