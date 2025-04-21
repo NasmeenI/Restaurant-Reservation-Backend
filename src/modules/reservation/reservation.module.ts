faa675cdd5d954887a5eb7c5eb229f90516925ce
ThanaphomH <thanaphom.h@gmail.com>
2025-04-21 22:04:15 +0700

    fix: import twilio module in reservation module

--- src/modules/reservation/reservation.module.ts
+++ src/modules/reservation/reservation.module.ts
@@ -3,13 +3,12 @@ import { ReservationService } from './reservation.service';
 import { ReservationReminderService } from './reservation-reminder.service';
 import { ReservationController } from './reservation.controller';
 import { RepositoryModule } from 'src/database/repository.module';
+import { TwilioModule } from 'src/modules/twilio/twilio.module';
+import { TwilioService } from 'src/modules/twilio/twilio.service';
 
 @Module({
-  imports: [RepositoryModule],
+  imports: [RepositoryModule, TwilioModule],
   controllers: [ReservationController],
-  providers: [
-    ReservationService,
-    ReservationReminderService,
-  ],
+  providers: [ReservationService, ReservationReminderService, TwilioService],
 })
 export class ReservationModule {}
