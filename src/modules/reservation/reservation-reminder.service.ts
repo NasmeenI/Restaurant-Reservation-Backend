29c4c05ab4e42e486f2739dc3cc034501bd39f66
NasmeenI <nasmeen2503@gmail.com>
2025-04-22 00:48:28 +0700

    fix: reminder period to 1 hour

--- src/modules/reservation/reservation-reminder.service.ts
+++ src/modules/reservation/reservation-reminder.service.ts
@@ -20,7 +20,7 @@ export class ReservationReminderService {
   async handleReminder() {
 
     const now = moment();
-    const oneHourFromNow = moment().add(59, 'minutes');
+    const oneHourFromNow = moment().add(1, 'hours');
     console.log(`Checking reservation reminders from ${now.format('HH:mm')} to ${oneHourFromNow.format('HH:mm')}`);
     const upcomingReservations = await this.reservationService.findByStartTimeRange(
       now.toDate(),
